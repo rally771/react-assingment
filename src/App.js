70ccdb22e3ee7ba020b2b9113a6886e6d585d2b8
rally771 <yashwith989@gmail.com>
2019-04-25 11:27:48 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,7 +6,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div className="main-background">
+      <div className="container">
+      <div className="col-md-12">
+      <div className="main-form-background">
       <Text/>
+      </div>
+      </div>
+      </div>
+      </div>
+      
     );
   }
 }
--- src/text.jsx
+++ src/text.jsx
@@ -1,109 +1,108 @@
 import React, { Component } from 'react'
 export default class Text extends Component {
+  constructor(props){
+    super();
+    this.state = {
+      textarea : '', //Save state of paragraph
+      textbox  : '',//Save state of input
+      found : [],//if found save values
+      notfound:'',//output for notfound
+      stringvalue:[]//to display sentence
+    }
+    this.baseState = this.state ={
+      found : [],//if found save values
+      notfound:'',//output for notfound
+      stringvalue:[]//to display sentence
+    }
+  }
+  resetForm = () => {
+    this.setState(this.baseState)
+  }
     handleChange = (e) => {
         this.setState({
             [e.target.name] : e.target.value
-        })
+        }) 
     }
 
-    findingString = (e) => {
+    findingString = async (e) => {
         e.preventDefault();
         const textarea = this.state.textarea;
         const string = this.state.textbox;
-        let sentence = [];
         let i = 0;
-        let position = textarea.indexOf(string);
-        let sentences = textarea.split("\n");
-        // const sentenceArray = Object.keys(sentences).map(i => sentences[i]);
-        // console.log("sentence in textarea",typeof(sentences));
-        // console.log("sentence in string",typeof(sentenceArray));
-        // console.log("sentence in textarea",sentences);
-        // console.log("sentence in textarea",sentenceArray);
-
-        const keys = Object.values(sentences)
+        let sentences = textarea.toLowerCase().split("\n");// split the object from endline
+        let multiplestring = string.toLowerCase().split(" ");// split inut search word to search mutiple words
+        
+        console.log(multiplestring);
+        const keys = Object.values(sentences) // got values from the splitted sentence
 for (const key of keys) {
   i++;
-  // console.log("for loop keys",key)
-  let keyvalue = key.split(" ");
+  
+  let keyvalue = key.split(" "); // get words from sentence
   console.log("keyvalue",keyvalue);
-  let keyvalues = Object.values(keyvalue);
-  console.log("keyvalues",keyvalues)
-  for(const keyval of keyvalues){
-    if (keyval === string) {
-      var themainvalue = "the word " + string +"is in line no " + i;
-      document.getElementById("mainh1").innerHTML= themainvalue;
+  let keyvalues = Object.values(keyvalue); //save values 
+  
+  let multiplestringvalues = Object.values(multiplestring); // for multiple words
+  for(const keyval of keyvalues ){ //
+    for(const multiplestringval of multiplestringvalues){
+    if (keyval === multiplestringval) {
+      await this.setState({
+       found : this.state.found.concat(i),
+         stringvalue :this.state.stringvalue.concat( Object.values(sentences)[i-1]) // to display sentence 
+      });
+     
+      console.log("sentence" ,Object.entries(sentences)[i-1]); // to display sentence 
+      console.log("founded at", this.state.found)
     }
     else{
-      console.log("key values inside if", typeof(string),string)
-      console.log("not found in line no ",i)
+      
     }
-    
   }
+  }
+  await this.setState({
+        notfound : this.state.notfound.concat("not found")
+       });
   
 }
-
-        
-        //  console.log("position of string in textarea",position);
-
-        //  for(sentence in sentenceArray){
-        //    console.log(sentence);
-        //  }
-
-         
-
-
-
-
-
-        // // while(countLine < position){
-        // //  let countLine1 = textarea.indexOf("\n", countLine + 1);
-        // //  countLine = countLine1;
-        // //  console.log("counting line " , countLine1);
-        // //  count++;
-        // // }
-        // console.log("line of string is :", count);
-
-
-    // //determine what the fontsize will be
-    // let fontsize = 12;
-    // //get number of characters that can fit in a row
-    // let charsperrow = textarea.clientWidth / fontsize;
-    // //get any hard returns
-    // let hardreturns = textarea.textContent.split(/\r|\r\n|\n/);
-    // let rows = hardreturns.length;
-    // //loop through returns and calculate soft returns
-    // for(let i = 0,len = rows; i < len; i++){
-    //     let line = hardreturns[i];
-    //     let softreturns = Math.round(line.length / charsperrow);
-    //     //if softreturns is greater than 0, minus by 1 (hard return already counted)
-    //     softreturns = Math.round(softreturns > 0 ? (softreturns - 1) : 0);
-    //     rows += softreturns;
-    // }
-    // console.log(Math.round(rows));
-
-
-
-
         
     }
 
   render() {
+    const { found } = this.state;
+    const { notfound } = this.state;
+    const { stringvalue } = this.state;
     // console.log(this.state,"this.state")
     return (
+      
       <div className="form-main"> 
+      
         <form onSubmit = {this.findingString}>
             <div className="form-group">
-                <label htmlFor="exampleFormControlTextarea1">Example textarea</label><br/>
-                <textarea   className="form-control text-area-form" name = "textarea" onChange = {this.handleChange} id="exampleFormControlTextarea1" rows="3" cols="40"></textarea>
+                <label htmlFor="exampleFormControlTextarea1">Please Enter The Paragraph with endline </label><br/>
+                <textarea value = {this.state.textarea}  className="form-control text-area-form" name = "textarea" onChange = {this.handleChange} id="exampleFormControlTextarea1" rows="3" cols="40"></textarea>
             </div>
             <div className="form-group">
-                <label htmlFor="inputtext"></label><br />
-                <input onChange = {this.handleChange} name = "textbox" className="form-control text-form" id="inputtext" type="text" placeholder="Default input" />
+                <label htmlFor="inputtext">Please enter words to search for</label><br />
+                <input onChange = {this.handleChange} value = {this.state.textbox} name = "textbox" className="form-control text-form" id="inputtext" type="text" placeholder="Default input" />
             </div>
             <button type="button" onClick = {this.findingString} className="btn btn-primary button">Find Text</button>
+            <button type="button" onClick = {this.resetForm} className="btn btn-primary button clear-button">Clear Search Results</button>
+            <br/>
+            <div className="resume"><a href ="https://drive.google.com/open?id=1L49lOZZVbAFz9yJG2BaMKMmHe8gTvtM7">Download Resume</a></div>
+        <br/>
         </form>
-        <h1 id ="mainh1"></h1>
-      </div>
+        
+        {
+          found.length > 0 ? 
+            <p id ="mainh1">The words found at line {found.map(at => (at + " "))}</p> :
+            <p id ="mainh1">{notfound}</p>
+        }
+        {
+          found.length && stringvalue.length> 0 ? 
+            found.map(at => <p id ="mainh1">The words  found at {at}</p>) && stringvalue.map(att => <p id ="mainh1"> {att}</p>) :
+            <p id ="mainh1">{notfound}</p>
+        }
+        </div>
+     
     )
   }
 }
